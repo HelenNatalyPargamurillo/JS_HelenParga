e3874c1e9dc3f3bfe74de878eb565ad582dc23f0
helen <hnmuurillo24@gmail.com>
2024-04-11 08:16:59 -0500

    feat: :sparkles: tipos de funciones

--- scrip.js
+++ scrip.js
@@ -2,3 +2,24 @@ console.log("Hola Mundo");
  a=2
  b=6
  console.log(a+b)
+ //**************** FUCION CON RETORNO Y CON PARAMETROS ********* */
+ function funcionNormal(){
+    console.log ("a+b");
+
+ }
+ suma(5,7)
+
+  //**************** FUCION SIN RETORNO Y SIN PARAMETROS ********* */
+  function funcionNormal(){
+    console.log ("a+b");
+    return a+b;
+
+ }
+ suma(5,7)
+ console.log(suma)
+
+
+ 
+
+  
+
