25c7cf940c71c86cbd9075587a190dd4a676fc92
helen <hnmuurillo24@gmail.com>
2024-04-11 09:05:04 -0500

    feat: :zap: ejercicio en clase
    desarrollo del ejercicio  puesto en clase

--- scrip.js
+++ scrip.js
@@ -17,6 +17,26 @@ console.log("Hola Mundo");
  }
  suma(5,7)
  console.log(suma)
+ // ** FUNCION SIN RETORNO Y SIN PARAMETROS **
+function funcionNormal(){
+    console.log("Mi función");
+}
+
+// ** FUNCION SIN RETORNO Y CON PARAMETROS **
+function suma(a,b){
+    console.log(a+b);
+}
+// ** FUNCION CON RETORNO Y CON PARAMETROS **
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b;
+}
+// ** FUNCION CON RETORNO Y SIN PARAMETROS **
+function salonFavorito(){
+    //console.log(a+b);
+    return "P1";
+}
+
 
  //**************** BUCLES FOR ********* */
  for(let i=0;i<10;i=i+1){
@@ -31,6 +51,19 @@ console.log("Hola Mundo");
  for (let i=0;i<tamano;i++){
     console.log(arreglo[i])
  }
+
+//**************** EJERCICIO ********* */
+
+
+
+console.log("ingrese valor celcius")
+
+var celcius =20
+
+var farenheit =32+(9*celcius/5)
+
+console.log(farenheit)
+
   
 
 
---
+++ scrpt.js
@@ -0,0 +1,14 @@
+//**************** EJERCICIO ********* */
+
+
+
+console.log("ingrese valor celcius")
+
+var celcius =20
+
+var farenheit =32+(9*celcius/5)
+
+console.log(farenheit)
+
+  
+//creado por: Helen Nataly Parga Murillo
