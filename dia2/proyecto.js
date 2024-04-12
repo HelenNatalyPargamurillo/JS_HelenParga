09cf2f07859d8d9c905395e4b694e5fa8392c506
helen <hnmurillo24@gmail.com>
2024-04-12 08:59:33 -0500

    feat: :sparkles: creacion proyecto
    proyecto del dia 2 ejercicios

---
+++ dia2/proyecto.js
@@ -0,0 +1,35 @@
+let est = "0"
+let tel = 0
+let acu = "0"
+let drc = "0"
+let id = 0
+let ap = "0"
+let nb = "0"
+let opcion = "0"
+    opcion =prompt ("menu \n1.registrar camper \n2.registrar trainer \n3.rutas \n4.estados \n5.reportes ")
+
+if (opcion =="1") { 
+    nb = prompt("1.Nombre camper");
+    ap=prompt("2.apellido");
+    id=prompt("3. numero de identificacion");
+    drc=prompt("4. direccion");
+    acu=prompt("5. acudiente");
+    tel=prompt("6. telefono");
+    est=prompt("7. estado")
+
+
+ } 
+ if (opcion =="2") { 
+    registrop = prompt("1.Registrar Trainer");
+
+ } 
+ if (opcion =="3") { 
+    registro = prompt("1.Rutas");
+
+ } 
+ if (opcion =="1") { 
+    registro = prompt("1.Registrar Camper");
+
+ } 
+  
+
