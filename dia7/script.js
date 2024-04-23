7209934e76978fa04a0f55f527341c87c46368a6
helen <hnmurillo24@gmail.com>
2024-04-23 09:50:18 -0500

    feat: :sparkles: quizz
    desarollo

---
+++ dia7/script.js
@@ -0,0 +1,19 @@
+function fetchLuke(){
+    let xhr =HMLHttproquest
+    let url=`https://swapi.py4e.com/api/people/1/`
+    xhr.open("GET",url,true);
+    xhr.onreadystatechange=funcion(){
+        if(this .radyState === 4 && this.status === 200){
+        let response =JSON.parse(this.responseText);
+        console.log(response);
+        displayjson(response);
+        } else if (this.radyState ===4){
+            console.log(`Error:`,this.StatusText)
+        }
+    };
+    xhr.send();
+}
+ function displayHer(data){
+    let jsoninfo=document.get.ElementById("TheStarWars")
+ }
+ TheStarWars 
