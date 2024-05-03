dffcdbd33a99bdfb14ebee84365c2e873f8a6679
helen <hnmurillo24@gmail.com>
2024-05-03 00:05:39 -0500

    feat: :wrench: arreglo codigo
    correciiones

--- dia6/script.js
+++ dia6/script.js
@@ -1,53 +1,29 @@
-const JSON = {
-	"response": "success",
-	"id": "2",
-	"name": "Abe Sapien",
-	"powerstats": {
-		"intelligence": "88",
-		"strength": "28",
-		"speed": "35",
-		"durability": "65",
-		"power": "100",
-		"combat": "85"
-	},
-	"biography": {
-		"full-name": "Abraham Sapien",
-		"alter-egos": "No alter egos found.",
-		"aliases": [
-			"Langdon Everett Caul",
-			"Abraham Sapien",
-			"Langdon Caul"
-		],
-		"place-of-birth": "-",
-		"first-appearance": "Hellboy: Seed of Destruction (1993)",
-		"publisher": "Dark Horse Comics",
-		"alignment": "good"
-	},
-	"appearance": {
-		"gender": "Male",
-		"race": "Icthyo Sapien",
-		"height": [
-			"6'3",
-			"191 cm"
-		],
-		"weight": [
-			"145 lb",
-			"65 kg"
-		],
-		"eye-color": "Blue",
-		"hair-color": "No Hair"
-	},
-	"work": {
-		"occupation": "Paranormal Investigator",
-		"base": "-"
-	},
-	"connections": {
-		"group-affiliation": "Bureau for Paranormal Research and Defense",
-		"relatives": "Edith Howard (wife, deceased)"
-	},
-	"image": {
-		"url": "https:\/\/www.superherodb.com\/pictures2\/portraits\/10\/100\/956.jpg"
+function fetchSuperHero(){
+	let xhr = new XMLHttpRequest();
+	let heroID =document.getElementById("heroid").value;
+	let apiKey="70e2818ad0610b19bfa61d614778a885"
+	let url = `https://superheroapi.com/api.php/${apiKey}/${heroID}`;
+	xhr.open("GET".url,true);
+	xhr.onreadystatechange=function(){
+		if(this.readyState ===4 && this.status === 200){
+			let response = JSON.parse (this.responseText);
+			console.log(response);
+			displayHero(response);
+		} else if (this.readyState ==4){
+			console.log("Error:",this.statusText);
+			
+		
+		}
+	};
+	xhr.send();
+}
+function displayHero(data){
+	let heroInfo = documen.getElementById("superHeroInfo");
+	if(data.response === "error"){
+		heroInfo.innerHTML=` <p>Error:${data.error}</p>`; 
+	}else{
+		heroInfo.innerHTML=`
+		<p>Name:${data.name}</p>
+		<p>inteligencia :${data.powestats} </p>`
 	}
 }
-
-console.log(JSON["image"]);
