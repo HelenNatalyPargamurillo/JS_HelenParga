3036dccfd7c853988087d4c5eb966fc77bdf8eee
helen <hnmurillo24@gmail.com>
2024-05-20 08:59:50 -0500

    feat: :bug: correciones
    correccion ejercicio dia 6

--- dia6/script.js
+++ dia6/script.js
@@ -3,7 +3,7 @@ function fetchSuperHero(){
 	let heroID =document.getElementById("heroid").value;
 	let apiKey="70e2818ad0610b19bfa61d614778a885"
 	let url = `https://superheroapi.com/api.php/${apiKey}/${heroID}`;
-	xhr.open("GET".url,true);
+	xhr.open("GET",url,true);
 	xhr.onreadystatechange=function(){
 		if(this.readyState ===4 && this.status === 200){
 			let response = JSON.parse (this.responseText);
@@ -18,12 +18,23 @@ function fetchSuperHero(){
 	xhr.send();
 }
 function displayHero(data){
-	let heroInfo = documen.getElementById("superHeroInfo");
+	let heroInfo = document.getElementById("SuperHeroInfo");
 	if(data.response === "error"){
 		heroInfo.innerHTML=` <p>Error:${data.error}</p>`; 
 	}else{
 		heroInfo.innerHTML=`
 		<p>Name:${data.name}</p>
-		<p>inteligencia :${data.powestats} </p>`
+		<p>inteligencia :${data.powerstats["intelligence"]} </p>
+		<p>biography:${data.biography["full-name"]} </p>
+		<p>appearance:${data.appearance["gender"]} </p>
+		<p>work:${data.work["occupation"]} </p>
+		<p>connections :${data.connections["group-affiliation"]} </p>
+		<img src="${data.image["url"]}"></img>`
+		
+		
 	}
 }
+
+
+
+
